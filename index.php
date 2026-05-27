7d62b8fa686d3ae4b065ffac1511033c397667cd
Adebbayo Damilare <damilareomoadebayo@gmail.com>
2026-05-27 09:21:34 +0100

    fix php routing

---
+++ index.php
@@ -0,0 +1,9 @@
+cat > /Users/mac/Downloads/pappy/router.php << 'EOF'
+<?php
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$file = __DIR__ . $path;
+if (is_file($file)) {
+    return false;
+}
+require __DIR__ . '/webhook.php';
+EOF
